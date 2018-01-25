7362355acba3073a38d5b5cc183628f9351c1de4
REZA <hamidmick@yahoo.com>
2018-01-25 16:21:33 -0500

    new class main

---
+++ remoteexe/src/com/hamid/eclipse/Mainexe.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.hamid.eclipse;
+
+/**
+ * @author Reza
+ *
+ */
+public class Mainexe {
+
+	/**
+	 * 
+	 */
+	public Mainexe() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
